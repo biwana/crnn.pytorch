8c6f069770ae9cc056a68112b6009c20b208d60b
Brian Iwana <biwana@gmail.com>
2019-03-03 07:17:00 +0900

    titles

---
+++ test.py
@@ -0,0 +1,42 @@
+import torch
+from torch.autograd import Variable
+import utils
+import dataset
+from PIL import Image
+import numpy as np
+
+import models.crnn as crnn
+
+
+model_path = './data/crnn.pth'
+alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUV'
+labels = np.genfromtxt("title30cat-east-cut-test.txt")[:5]
+
+model = crnn.CRNN(32, 1, 37, 256)
+if torch.cuda.is_available():
+    model = model.cuda()
+print('loading pretrained model from %s' % model_path)
+model.load_state_dict(torch.load(model_path))
+
+converter = utils.strLabelConverter(alphabet)
+
+transformer = dataset.resizeNormalize((100, 32))
+for l in labels:
+    img_path = './data/demo.png'
+    image = Image.open(l).convert('L')
+    image = transformer(image)
+    if torch.cuda.is_available():
+        image = image.cuda()
+    image = image.view(1, *image.size())
+    image = Variable(image)
+
+    model.eval()
+    preds = model(image)
+
+    _, preds = preds.max(2)
+    preds = preds.transpose(1, 0).contiguous().view(-1)
+
+    preds_size = Variable(torch.IntTensor([preds.size(0)]))
+    raw_pred = converter.decode(preds.data, preds_size.data, raw=True)
+    sim_pred = converter.decode(preds.data, preds_size.data, raw=False)
+    print('%-20s => %-20s' % (raw_pred, sim_pred))
